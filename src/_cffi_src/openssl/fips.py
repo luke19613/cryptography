2f1ffef9a921fa042d98c10f71762d1486342521
Luke <Luke>
2016-10-30 16:27:33 -0400

    cryptography-FIPS

---
+++ src/_cffi_src/openssl/fips.py
@@ -0,0 +1,21 @@
+
+from __future__ import absolute_import, division, print_function
+
+INCLUDES = """
+"""
+
+TYPES = """
+"""
+
+FUNCTIONS = """
+int FIPS_mode(void);
+int FIPS_mode_set(int r);
+"""
+
+MACROS = """
+"""
+
+CUSTOMIZATIONS = """
+"""
+
+
